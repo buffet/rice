c7b1ec185e933b9cd63a15af0d4e908718ca2c18
meyni <niclas@niclasmeyer.berlin>
2018-01-09 11:58:51 +0100

    ui mode

--- color_stuff.sh
+++ color_stuff.sh
@@ -1,8 +1,8 @@
 #! /bin/env bash
 
-apiaw=$(curl -sd '{"model":"default"}' http://colormind.io/api/ | # get json
-    sed -E 's/.*:\[\[(.*)\]\]\}$/\1/'                               | # extract array
-    sed -E 's/\],\[/;/g')                                         # make seperated
+apiaw=$(curl -sd '{"model":"ui"}' http://colormind.io/api/ | # get json
+    sed -E 's/.*:\[\[(.*)\]\]\}$/\1/'                      | # extract array
+    sed -E 's/\],\[/;/g')                                    # make seperated
 
 # to array
 IFS=';' read -r -a colors <<< "$apiaw"
@@ -21,21 +21,21 @@ do
 done
 
 # background
-echo "Background: ${colors[0]}"
-xsetroot -solid "${colors[0]}"
+echo "Background: ${colors[4]}"
+xsetroot -solid "${colors[4]}"
 
 # normal window
-echo "Normal window border: ${colors[1]}"
-bspc config normal_border_color "${colors[1]}"
+echo "Normal window border: ${colors[3]}"
+bspc config normal_border_color "${colors[3]}"
 
 # focused window
 echo "Focused window border: ${colors[2]}"
 bspc config focused_border_color "${colors[2]}"
 
 # presel color
-echo "Presel color: ${colors[3]}"
-bspc config presel_feedback_color "${colors[3]}"
+echo "Presel color: ${colors[1]}"
+bspc config presel_feedback_color "${colors[1]}"
 
 # unused
-echo "Unused: ${colors[4]}"
+echo "Unused: ${colors[0]}"
 
