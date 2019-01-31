c2b4670252cfacfce9b0066d6bd573e5dfe733b2
buffet <niclas@countingsort.com>
2019-01-31 22:38:59 +0100

    XMonad

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -213,4 +213,13 @@ in
       v = "nvim";
     };
   };
+
+  xsession = {
+    enable = true;
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      config = ../xmonad/xmonad.hs;
+    };
+  };
 }
---
+++ nixos/programs/dwm.nix
@@ -0,0 +1,6 @@
+{ config, lib, pkgs, ... }:
+
+{
+  #config.programs.dwm.enable = true;
+  pkgs.dwm.enable = true;
+}
