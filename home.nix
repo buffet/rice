485fe5a74aa6e912d9f86b50c5da4354e88c770a
buffet <niclas@countingsort.com>
2019-03-06 11:00:44 +0100

    Got rid of tmux

--- home.nix
+++ home.nix
@@ -74,11 +74,6 @@ in {
       mkdir = "mkdir -p";
       ra = "ranger";
       t = "cd /tmp";
-      ta = "tmux attach-session -t";
-      tkill = "tmux kill-session -t";
-      tls = "tmux list-sessions";
-      tm = "tmux";
-      tnew = "tmux new -s";
       v = "nvim";
     };
     shellOptions = [
@@ -123,41 +118,6 @@ in {
     userName = "buffet";
   };
 
-  programs.tmux = {
-    enable = true;
-    extraConfig = ''
-      # New prefix
-      set -g prefix C-a
-      unbind C-b
-      bind-key C-a send-prefix
-
-      # Smart pane switching with awareness of Vim splits.
-      # See: https://github.com/christoomey/vim-tmux-navigator
-      is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
-      | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
-      bind-key -n C-h if-shell "$is_vim" "send-keys C-h"  "select-pane -L"
-      bind-key -n C-j if-shell "$is_vim" "send-keys C-j"  "select-pane -D"
-      bind-key -n C-k if-shell "$is_vim" "send-keys C-k"  "select-pane -U"
-      bind-key -n C-l if-shell "$is_vim" "send-keys C-l"  "select-pane -R"
-      bind-key -n C-\ if-shell "$is_vim" "send-keys C-\\" "select-pane -l"
-      bind-key -T copy-mode-vi C-h select-pane -L
-      bind-key -T copy-mode-vi C-j select-pane -D
-      bind-key -T copy-mode-vi C-k select-pane -U
-      bind-key -T copy-mode-vi C-l select-pane -R
-      bind-key -T copy-mode-vi C-\ select-pane -l
-
-      # Easier splitting
-      bind | split-window -h
-      bind - split-window -v
-
-      # No status bar
-      set -g status off
-
-      # st fix
-      set -as terminal-overrides ',st*:Ss@'
-    '';
-  };
-
   programs.neovim = {
     enable = true;
     configure = {
@@ -176,7 +136,6 @@ in {
           supertab
           tabular
           vim-easytags
-          vim-tmux-navigator
           zoomwintab-vim
         ];
       };
