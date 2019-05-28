eb4937204d27abfc6d8c4f1ac49dfb9cb20ccad8
buffet <niclas@countingsort.com>
2019-05-28 15:12:42 +0200

    Set promptcolor

--- home.nix
+++ home.nix
@@ -72,8 +72,8 @@ in {
 
         prompt() {
             case $? in
-                0) PS1='%' ;;
-                *) PS1='\[\e[31m\]%' ;;
+                0) PS1='\[\e[36m\]\$' ;;
+                *) PS1='\[\e[31m\]\$' ;;
             esac
 
             [[ $IN_NIX_SHELL ]] && PS1+="'"
