403f973e11b5d5a2e7f4503f8e29f54f261c4508
buffet <niclas@countingsort.com>
2020-07-26 16:57:30 +0000

    Add local, newer copy of kak-fzf

--- overlay/default.nix
+++ overlay/default.nix
@@ -2,4 +2,5 @@ self: super:
 {
   grimshot = super.callPackage ./grimshot.nix { };
   kak-attach-session = super.callPackage ./kak-attach-session.nix { };
+  kakounePlugins = super.kakounePlugins // { kak-fzf = super.callPackage ./kak-fzf.nix { }; };
 }
