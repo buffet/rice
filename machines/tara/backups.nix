0342ba9a9d2f245a4ac21abc714141473c447fbf
buffet <niclas@countingsort.com>
2020-09-03 22:20:00 +0000

    Fix backup before and after

--- machines/tara/backups.nix
+++ machines/tara/backups.nix
@@ -22,60 +22,78 @@ in
     };
   };
 
-  systemd.services = {
-    borgbackup-job-backups.after = [ "postgresql-dump.service" ];
+  systemd = {
+    services = {
+      borgbackup-job-backup = {
+        after = [ "postgresql-dump.service" ];
+        before = [ "rclone-sync-backups.service" ];
+        wants = [ "postgresql-dump.service" "rclone-sync-backups.service" ];
+      };
 
-    postgresql-dump = {
-      description = "postgresql backup";
-      after = [ "postgresql.service" ];
-      requires = [ "postgresql.service" ];
-
-      script = let
-        location = "/var/backup/postgrespql";
+      postgresql-dump = let
+        location = "/var/backup/postgresql";
         postgres = config.services.postgresql;
       in
-        ''
-          umask 0077 # backup only readable by postgres user
+        {
+          description = "postgresql backup";
+          after = [ "postgresql.service" ];
+          requires = [ "postgresql.service" ];
 
-          if [ -e ${location}/dump.sql.gz ]; then
-              mv ${location}/dump.sql.gz ${location}/dump.old.sql.gz
-          fi
+          preStart = ''
+            mkdir -p "${location}"
+            chown postgres:postgres "${location}"
+            chmod 770 "${location}"
+          '';
 
-          ${postgres.package}/bin/pg_dumpall | \
-              ${pkgs.gzip}/bin/gzip -c >${location}/dump.sql.gz
+          script = ''
+            umask 0077 # backup only readable by postgres user
+
+            if [ -e "${location}/dump.sql.gz" ]; then
+                mv "${location}/dump.sql.gz" "${location}/dump.old.sql.gz"
+            fi
+
+            ${postgres.package}/bin/pg_dumpall | \
+                ${pkgs.gzip}/bin/gzip -c >"${location}/dump.sql.gz"
+          '';
+
+          serviceConfig = {
+            User = "postgres";
+            Type = "oneshot";
+            PermissionsStartOnly = true;
+          };
+        };
+
+      rclone-sync-backups = let
+        makeRemoteConfig = remote: with remote; ''
+          [remote]
+          type = ${type}
+          env_auth = ${if env_auth then "true" else "false"}
+          access_key_id = ${access_key_id}
+          secret_access_key = ${secret_access_key}
+          region = ${region}
+          endpoint = ${endpoint}
+          location_contraint = ${location_contraint}
         '';
 
-      serviceConfig = {
-        User = "postgres";
-        Type = "oneshot";
-      };
+        configPath = pkgs.writeText "rclone.conf" (makeRemoteConfig (import ../../secrets/wasabi.nix));
+        bucket = "buffet-backups";
+      in
+        {
+          description = "Sync backups to S3 remote";
+          after = [ "network.target" ];
+          requires = [ "network.target" ];
+
+          serviceConfig = {
+            ExecStart = "${pkgs.rclone}/bin/rclone sync --config ${configPath} \"${location}\" remote:${bucket}";
+            Type = "oneshot";
+          };
+        };
     };
 
-    rclone-sync-backups = let
-      makeRemoteConfig = remote: with remote; ''
-        [remote]
-        type = ${type}
-        env_auth = ${if env_auth then "true" else "false"}
-        access_key_id = ${access_key_id}
-        secret_access_key = ${secret_access_key}
-        region = ${region}
-        endpoint = ${endpoint}
-        location_contraint = ${location_contraint}
-      '';
-
-      configPath = pkgs.writeText "rclone.conf" (makeRemoteConfig (import ../../secrets/wasabi.nix));
-      bucket = "buffet-backups";
-    in
-      {
-        description = "Sync backups to S3 remote";
-        after = [ "borgbackup-job-backups.service" "network.target" ];
-        requires = [ "network.target" ];
-
-        serviceConfig = {
-          ExecStart = "${pkgs.rclone}/bin/rclone sync --config ${configPath} \"${location}\" remote:${bucket}";
-          User = "borg";
-          Type = "oneshot";
-        };
-      };
+    timers.rclone-sync-backups = {
+      wantedBy = [ "timers.target" ];
+      partOf = [ "rclone-sync-backups.service" ];
+      timerConfig.OnCalendar = "daily";
+    };
   };
 }
