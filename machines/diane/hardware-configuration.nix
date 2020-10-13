793d4428f7364091d086150de85cf20ad8fb2319
buffet <niclas@countingsort.com>
2020-10-13 13:55:49 +0000

    Revert "Add diane"
    This reverts commit 460379dd55f652b9747eb3fd0ea836dc7ba9d855.

--- machines/diane/hardware-configuration.nix
+++
@@ -1,6 +0,0 @@
-{ ... }:
-{
-  imports = [ <nixpkgs/nixos/modules/profiles/qemu-guest.nix> ];
-  boot.loader.grub.device = "/dev/sda";
-  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
-}
