a48e2c229e933d5a50befd66ffa6f2386afcf342
buffet <niclas@countingsort.com>
2020-10-24 16:35:35 +0000

    Add plover scratchpad

--- modules/desktop/xmonad/xmonadhs.nix
+++ modules/desktop/xmonad/xmonadhs.nix
@@ -8,20 +8,25 @@
   import XMonad.Layout.Reflect
   import XMonad.Layout.Spacing
   import XMonad.Util.EZConfig
+  import XMonad.Util.NamedScratchpad
   import XMonad.Util.SpawnOnce
 
   main = xmonad =<< bar myConfig
 
   myConfig = flip additionalKeysP myKeys $ ewmh $ def
-      { modMask  = mod4Mask
-      , terminal = "alacritty"
+      { modMask     = mod4Mask
+      , terminal    = "alacritty"
       , borderWidth = 0
 
       , layoutHook  = myLayoutHook
+      , manageHook  = myManageHook
       , startupHook = myStartupHook
       }
 
-  bar = statusBar "${pkgs.xmobar}/bin/xmobar ${xmobarrc}" xmobarPP toggleStrutsKey
+  scratchpads = [ NS "plover" "plover" (title =? "Plover") defaultFloating
+                ]
+
+  bar = statusBar "${pkgs.xmobar}/bin/xmobar ${xmobarrc}" (namedScratchpadFilterOutWorkspacePP xmobarPP) toggleStrutsKey
       where
           toggleStrutsKey XConfig{modMask = modm} = (modm, xK_b)
           xmobarPP = def
@@ -38,6 +43,7 @@
            , ("M-S-x",      spawn "${pkgs.i3lock}/bin/i3lock -ec '${colors.primary.background}'")
            , ("M-p",        spawn "${pkgs.maim}/bin/maim -s | xclip -i -sel c -t image/png")
            , ("M-S-p",      spawn "${pkgs.maim}/bin/maim | xclip -i -sel c -t image/png")
+           , ("M-s M-p",    namedScratchpadAction scratchpads "plover")
            ]
 
   myLayoutHook = tall ||| Full
@@ -47,6 +53,8 @@
   applySpacing size = spacingRaw False border True border True
       where
           border = (Border size size size size)
+
+  myManageHook = namedScratchpadManageHook scratchpads
 
   myStartupHook = do
       setWMName "LG3D"
