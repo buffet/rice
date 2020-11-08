cb2245980cc2813324cd179cdf4cb1bee5d92cbb
buffet <niclas@countingsort.com>
2020-11-08 13:47:55 +0000

    Start working on WsContexts

--- modules/desktop/xmonad/default.nix
+++ modules/desktop/xmonad/default.nix
@@ -2,6 +2,29 @@
 let
   cfg = config.buffet.desktop.xmonad;
   colors = config.buffet.desktop.colors;
+
+  readDirRec = dir:
+    lib.flatten (
+      lib.mapAttrsToList
+        (
+          name: value: let
+            path = builtins.toPath ((toString dir) + "/" + name);
+          in
+            if value == "directory" then readDirRec path else [ path ]
+        )
+        (builtins.readDir dir)
+    );
+
+  generateImports = dir: target:
+    builtins.listToAttrs (
+      map
+        (
+          p: lib.nameValuePair
+            (target + "/" + (lib.removePrefix ((toString dir) + "/") p))
+            { source = p; }
+        )
+        (lib.filter (lib.hasSuffix ".hs") (map toString (readDirRec dir)))
+    );
 in
   with lib; {
     options = {
@@ -12,13 +35,17 @@ in
 
     config = mkIf cfg.enable {
       buffet.home = {
+        home.file = {
+          ".xmonad/xmonad.hs".text = let
+            xmobarrc = pkgs.writeText "xmobarrc" (import ./xmobarrc.nix { inherit colors; });
+          in
+            import ./xmonadhs.nix { inherit pkgs colors xmobarrc; };
+        } // generateImports ./lib ".xmonad/lib";
+
+
         xsession.windowManager.xmonad = {
           enable = true;
           enableContribAndExtras = true;
-          config = let
-            xmobarrc = pkgs.writeText "xmobarrc" (import ./xmobarrc.nix { inherit colors; });
-          in
-            pkgs.writeText "xmonad.hs" (import ./xmonadhs.nix { inherit pkgs colors xmobarrc; });
         };
       };
     };
