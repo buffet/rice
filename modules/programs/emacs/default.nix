0a972f79811937e003fa197b599deb9cb1b790e6
buffet <niclas@countingsort.com>
2020-11-21 14:28:19 +0000

    Work on Emacs nix module

--- modules/programs/emacs/default.nix
+++ modules/programs/emacs/default.nix
@@ -2,9 +2,7 @@
 let
   cfg = config.buffet.programs.emacs;
   sources = import ../../../nix/sources.nix;
-  doom-emacs = pkgs.callPackage sources.nix-doom-emacs {
-    doomPrivateDir = ./doom.d;
-  };
+  doom-emacs = pkgs.callPackage sources.nix-doom-emacs;
 in
   with lib; {
     options = {
@@ -16,28 +14,31 @@ in
     config = mkIf cfg.enable {
       services.emacs = {
         enable = true;
-        package = doom-emacs;
         defaultEditor = true;
+
+        package = doom-emacs {
+          doomPrivateDir = ./doom.d;
+        };
       };
 
       buffet.home = {
-        home = {
-          sessionVariables = {
-            VISUAL = config.environment.variables.EDITOR;
-          };
+        home.sessionVariables = {
+          VISUAL = config.environment.variables.EDITOR;
+        };
 
-          # TODO: add this to Emacs' env
-          packages = with pkgs; [
-            clang-tools # lang/cc
+        home.packages = with pkgs; [
+            clang-tools # lang/cc +lsp
             fd
+            gitAndTools.gitflow # tools/magit
+            luajitPackages.moonscript # lang/lua +moonscript
             nixfmt # lang/nix
+            python37Packages.python-language-server # lang/python +lsp
             ripgrep
             rust-analyzer # lang/rust +lsp
             shellcheck # lang/sh
             texlab # lang/latex +lsp
             texlive.combined.scheme-full # lang/latex
-          ];
-        };
+        ];
       };
     };
   }
