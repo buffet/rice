975949df2d45576f3cac0574b754e8e3cf80896a
buffet <niclas@countingsort.com>
2021-05-21 23:29:00 +0000

    Theme awesome (mostly)

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -23,7 +23,7 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 require("awful.hotkeys_popup.keys")
 
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/mytheme/theme.lua")
 
 -- Init all modules (You can add/remove active modules here)
 require("modules.auto-start")
