86c894fbfed18f2740628cac5677b1d56046163b
buffet <niclas@countingsort.com>
2023-03-06 13:27:46 +0100

    feat(bash): improve C-o handling
    Signed-off-by: buffet <niclas@countingsort.com>

--- programs/bash.nix
+++ programs/bash.nix
@@ -38,7 +38,7 @@ in {
         }
         PROMPT_COMMAND="__prompt"
 
-        bind '"\C-o": "\C-a\C-k fg\n"'
+        bind '"\C-o": "\C-a\C-k fg; if [[ $? == 1 ]]; then nvim; fi\n"'
         bind '"\e\C-m": "\C-e | nvim\C-m"'
       '';
     };
