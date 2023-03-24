b1b4a71272b7f65f3ab4d5385c7a30b5035129d3
buffet <niclas@countingsort.com>
2023-03-24 12:32:02 +0100

    feat(bash): use 6 space indent prompt for now
    Signed-off-by: buffet <niclas@countingsort.com>

--- programs/bash.nix
+++ programs/bash.nix
@@ -30,13 +30,7 @@ in {
       ];
 
       initExtra = ''
-        __prompt() {
-            case $? in
-                0) PS1='\[\e[34m\]; \[\e[0m\]' ;;
-                *) PS1='\[\e[31m\]; \[\e[0m\]' ;;
-            esac
-        }
-        PROMPT_COMMAND="__prompt"
+        PS1='      '
 
         bind '"\C-o": "\C-a\C-k fg; if [[ $? == 1 ]]; then nvim; fi\n"'
         bind '"\e\C-m": "\C-e | nvim\C-m"'
