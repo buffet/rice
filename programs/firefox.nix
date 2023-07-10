62ab0c65d223e8e048ce93329d419e8193dabfe1
buffet <niclas@countingsort.com>
2023-07-10 14:47:29 +0200

    feat(firefox): switch to 10-ten-ja-reader
    Signed-off-by: buffet <niclas@countingsort.com>

--- programs/firefox.nix
+++ programs/firefox.nix
@@ -10,27 +10,13 @@
       package = pkgs.firefox-wayland;
 
       profiles."buffet" = {
-        extensions = with config.nur.repos.rycee.firefox-addons; let
-          # TODO: switch to yomitan
-          yomichan = buildFirefoxXpiAddon {
-            pname = "yomichan";
-            version = "20.5.22.1";
-            addonId = "alex@foosoft.net";
-            url = "https://addons.mozilla.org/firefox/downloads/file/3585060/yomichan-20.5.22.1.xpi";
-            sha256 = "sha256-/icvPD/nCJYS31owfYMD25QzFjsxAqapy/UAehhxsy8=";
-            meta = with lib; {
-              description = "Yomichan turns your browser into a tool for building Japanese language literacy by helping you to decipher texts which would be otherwise too difficult tackle. It features a robust dictionary with EPWING and flashcard creation support.";
-              license = licenses.gpl3;
-              platforms = platforms.all;
-            };
-          };
-        in [
+        extensions = with config.nur.repos.rycee.firefox-addons; [
+          config.nur.repos.rycee.firefox-addons."10ten-ja-reader"
           bitwarden
           istilldontcareaboutcookies
           refined-github
           sidebery
           ublock-origin
-          yomichan
         ];
 
         settings = {
