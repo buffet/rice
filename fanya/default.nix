c0ed1922301d91454221e355c447f6ba6042a88f
buffet <niclas@countingsort.com>
2023-08-13 19:23:41 +0200

    chore: format
    Signed-off-by: buffet <niclas@countingsort.com>

--- fanya/default.nix
+++ fanya/default.nix
@@ -23,7 +23,6 @@ in {
       settings.main.font = lib.mkForce "${theme.font.family}:size=${toString (theme.font.size - 1)}";
     };
 
-
     wayland.windowManager.sway = {
       config.gaps.inner = lib.mkForce 5;
     };
